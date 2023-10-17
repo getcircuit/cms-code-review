d7e210d7c74b7f76ea23b7d0fb52c9605beccd6a
vtrpldn <vitor@getcircuit.com>
2023-10-17 13:21:16 +0100

    Prepare project

--- README.md
+++
@@ -1,1 +0,0 @@
-# cms-code-review
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <iframe
+      src="https://getcircuit.com/package-tracker"
+      className="w-full h-screen"
+    />
+  );
+}
