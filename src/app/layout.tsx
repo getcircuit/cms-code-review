7a02df944f3a2a592062830b92d9e850dae69814
vtrpldn <vitor@paladini.dev>
2023-10-24 10:17:40 +0100

    Add new join page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "CMS Code Review Session",
-  description: "Let's work on some code together.",
+  title: "Circuit Package Tracker",
+  description: "Join now and enjoy early-bird benefits.",
 };
 
 export default function RootLayout({
@@ -16,6 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `(function(h,o,t,j,a,r){h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};h._hjSettings={hjid:2362275,hjsv:6};a=o.getElementsByTagName('head')[0];r=o.createElement('script');r.async=1;r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;a.appendChild(r);})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+        }}
+      />
       <body className={inter.className}>{children}</body>
     </html>
   );
